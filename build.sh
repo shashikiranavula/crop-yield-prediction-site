2348c44ce09811957ca1c5851fec2a47057701b2
shashikiranavula <shashikiranavula@users.noreply.github.com>
2026-04-18 02:08:22 +0530

    Initial production commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+echo "Downloading NLTK wordnet data..."
+python -m nltk.downloader wordnet
+
+echo "Running collectstatic..."
+python manage.py collectstatic --no-input
+
+echo "Applying database migrations..."
+python manage.py migrate
